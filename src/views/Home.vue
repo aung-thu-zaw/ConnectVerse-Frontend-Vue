657f865253b52391054c88db31283eb141bb0d5e
Aung Thu Zaw ( Xyder ) <105932994+aung-thu-zaw@users.noreply.github.com>
2024-04-11 13:09:35 +0630

    remove unnecessary files and components

---
+++ src/views/Home.vue
@@ -0,0 +1,5 @@
+<script lang="ts" setup></script>
+
+<template>
+  <h1 class="font-bold text-lg text-green-600">Hello World From Home Page</h1>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,51 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: {
+          background: '#FFFFFF',
+          text: '#333333',
+          primary: '#DCF8C6',
+          secondary: '#EAEAEA',
+          primaryText: '#000000',
+          secondaryText: '#333333',
+          accent: '#4CAF50'
+        },
+        dark: {
+          background: '#1E1E1E',
+          text: '#FFFFFF',
+          primary: '#DCF8C6',
+          secondary: '#333333',
+          primaryText: '#000000',
+          secondaryText: '#FFFFFF',
+          accent: '#4CAF50'
+        }
+      },
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px'
+      }
+    },
+    keyframes: {
+      zoomIn: {
+        from: { opacity: 0, transform: 'scale(0)' },
+        to: { opacity: 1, transform: 'scale(1)' }
+      },
+      zoomOut: {
+        from: { opacity: 1, transform: 'scale(1)' },
+        to: { opacity: 0, transform: 'scale(0)' }
+      }
+    },
+    animation: {
+      zoomIn: 'zoomIn 0.3s forwards',
+      zoomOut: 'zoomOut 0.3s forwards'
+    }
   },
-  plugins: [],
+  plugins: []
 }
-
